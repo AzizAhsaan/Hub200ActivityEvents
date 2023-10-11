f8c3b7ec2102cee09aeb63eac5c82bb02b96fbda
AzizAhsaan <azizahsaan339@gmail.com>
2023-10-11 11:57:07 +0300

    allfiles

--- Hub200ActivityEvents/settings.py
+++ Hub200ActivityEvents/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-duoxovi6o9zc7zs^zk(5eoxr!x=o7p@gb7eaj(f0o2j9=3%ft*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hub200activityevents.onrender.com']
 AUTH_USER_MODEL = 'ActivityEvents.CustomUser'
 
 # Application definition
