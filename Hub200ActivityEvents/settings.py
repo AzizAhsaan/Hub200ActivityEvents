f94e8b841ec70a227bd5bf32520705c7c73ae12d
AzizAhsaan <azizahsaan339@gmail.com>
2023-10-11 12:14:06 +0300

    allfiles

--- Hub200ActivityEvents/settings.py
+++ Hub200ActivityEvents/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
